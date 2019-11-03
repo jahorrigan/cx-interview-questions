636e4f8bb1a37b376819fda68e86b8686d0b7d54
jahorrigan <james@cloudambition.co.uk>
2019-11-03 08:53:19 +0000

    Add Negatively Priced Catalogue for Testing

--- shopping_basket/shopping_basket_tests/fixtures/catalogue.py
+++ shopping_basket/shopping_basket_tests/fixtures/catalogue.py
@@ -12,3 +12,12 @@ def base_products_catalogue():
     catalogue.add_new_product('Shampoo (Medium)', 2.50)
     catalogue.add_new_product('Shampoo (Large)', 3.50)    
     return catalogue
+
+@pytest.fixture
+def negative_products_catalogue():
+    """ Returns a catalogue with negative priced products """
+    catalogue = Catalogue()
+    catalogue.add_new_product('Jam', -0.50)
+    catalogue.add_new_product('Ice Cream', -1.79)
+    catalogue.add_new_product('Sardines', 1.89)
+    return catalogue
