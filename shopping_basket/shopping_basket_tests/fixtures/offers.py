09691db120a93375e195a43b82efe878d4014287
jahorrigan <james@cloudambition.co.uk>
2019-11-03 09:24:35 +0000

    Update Offer Fixture

--- shopping_basket/shopping_basket_tests/fixtures/offers.py
+++ shopping_basket/shopping_basket_tests/fixtures/offers.py
@@ -5,6 +5,6 @@ from shopping_basket import Offers
 def base_offers_list():
     """ Returns a base offers list containing two offers """
     offers_list = Offers()
-    offers_list.add_new_offer('Offer1', 'Baked Beans', 2, 1)
+    offers_list.add_new_offer('Offer1', 'Baked Beans', 3, 2)
     offers_list.add_new_offer('Offer2', 'Sardines', 1, 0.75)
     return offers_list
